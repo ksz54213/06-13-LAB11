807a706a39b90976009d3a9321e5a6865284dcb0
ksz54213 <36830376+ksz54213@users.noreply.github.com>
2018-06-13 14:59:36 +0800

    Add files via upload

---
+++ mcs_get.ino
@@ -0,0 +1,54 @@
+#include <LWiFi.h>
+#include "MCS.h"
+
+char _lwifi_ssid[] = "AA201-2G";
+char _lwifi_pass[] = "iteaa201";
+MCSDevice mcs("DuH3qJzm", "0rET0hA6xyVaXEcJ");
+
+MCSControllerOnOff LED("Control");
+MCSDisplayOnOff remote("LED2");
+
+void setup()
+{
+  Serial.begin(9600);
+
+  Serial.println("Connect to Wifi");
+  while (WiFi.begin(_lwifi_ssid, _lwifi_pass) != WL_CONNECTED) {
+    Serial.println("Wifi Reconnecting..");
+    delay(1000);
+  }
+
+  Serial.println("Connect to MCS...");
+  while (!mcs.connected()) {
+    Serial.println("MCS Reconnecting..");
+    mcs.connect();
+  }
+  Serial.println("MCS Connected!");
+  mcs.addChannel(LED);mcs.addChannel(remote);
+  pinMode(7, OUTPUT);
+  remote.set(0);
+}
+
+
+void loop()
+{
+  while (!mcs.connected()) {
+    mcs.connect();
+    if (mcs.connected()) {
+      Serial.println("MCS Reconnect");
+    }
+  }
+  mcs.process(1000);
+
+  if (LED.value() == 1) {
+    digitalWrite(7, HIGH);
+   remote.set(1);
+  } else {
+    digitalWrite(7, LOW);
+    remote.set(0);
+  }
+
+  delay(200);
+ 
+
+}
---
+++ mcs_post.ino
@@ -0,0 +1,49 @@
+#include <LWiFi.h>
+#include "MCS.h"
+
+char _lwifi_ssid[] = "AA201-2G";
+char _lwifi_pass[] = "iteaa201";
+MCSDevice mcs("DmmAd7ze", "3orms5gQCnSBr4TP");
+
+MCSDisplayInteger  Temp("R");
+
+void setup()
+{
+  Serial.begin(9600);
+
+  Serial.println("Connect to Wifi");
+  while (WiFi.begin(_lwifi_ssid, _lwifi_pass) != WL_CONNECTED) {
+    Serial.println("Wifi Reconnecting..");
+    delay(1000);
+  }
+
+  Serial.println("Connect to MCS...");
+  while (!mcs.connected()) {
+    Serial.println("MCS Reconnecting..");
+    mcs.connect();
+  }
+  Serial.println("MCS Connected!");
+
+  mcs.addChannel(Temp);
+
+}
+
+
+void loop()
+{
+  while (!mcs.connected()) {
+    mcs.connect();
+    if (mcs.connected()) {
+      Serial.println("MCS Reconnect");
+    }
+  }
+  mcs.process(1000);
+
+  int sensor = analogRead(3);
+  sensor/=4095;
+  sensor*=2.5;
+  
+  Temp.set(sensor);
+  Serial.println("Add sensor value.");
+  delay(200);
+}
